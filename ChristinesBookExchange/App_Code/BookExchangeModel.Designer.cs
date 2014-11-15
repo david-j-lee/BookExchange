55f02a2f44d2a98fe088a3152554782d4cde004f
SeaBear622 <davidlee@csu.fullerton.edu>
2014-11-15 12:34:04 -0800

    New Project
    Change Log:
    - Added a new empty project
    - Added data with entity
    - Started site master and styles

---
+++ ChristinesBookExchange/App_Code/BookExchangeModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\David\Documents\GitHub\BookExchange\ChristinesBookExchange\App_Code\BookExchangeModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ References/CREATE TABLE QUERY.sql
@@ -0,0 +1,228 @@
+USE [BookExchange]
+GO
+
+DROP TABLE Home
+DROP TABLE About
+DROP TABLE TradeRequests
+DROP TABLE UserReviews
+DROP TABLE Postings
+DROP TABLE Users
+
+/****** Object:  Table [dbo].[Home]    Script Date: 11/13/2014 7:54:08 PM ******/
+
+	SET ANSI_NULLS ON
+	GO
+	
+	SET QUOTED_IDENTIFIER ON
+	GO
+	
+	CREATE TABLE [dbo].[Home](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[MessageOne] [nvarchar](500) NULL,
+		[MessageTwo] [nvarchar](500) NULL,
+		[MessageThree] [nvarchar](500) NULL,
+		[ImageURLOne] [nvarchar](50) NULL,
+		[ImageURLTwo] [nvarchar](50) NULL,
+		[ImageURLThree] [nvarchar](50) NULL,
+		[EnteredOn] [datetime] NULL,
+		[EnteredBy] [int] NULL,
+		[UpdatedOn] [datetime] NULL,
+		[UpdatedBy] [int] NULL
+		) ON [PRIMARY]
+	GO
+	
+	ALTER TABLE [dbo].[Home]  
+		WITH CHECK ADD CONSTRAINT [FK_Home_Users] FOREIGN KEY([EnteredBy])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[Home] CHECK CONSTRAINT [FK_Home_Users]
+	GO
+
+	ALTER TABLE [dbo].[Home]  
+		WITH CHECK ADD CONSTRAINT [FK_Home_Users1] FOREIGN KEY([UpdatedBy])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[Home] CHECK CONSTRAINT [FK_Home_Users1]
+	GO
+
+/****** Object:  Table [dbo].[About]    Script Date: 11/13/2014 7:55:58 PM ******/
+
+	SET ANSI_NULLS ON
+	GO
+
+	SET QUOTED_IDENTIFIER ON
+	GO
+
+	CREATE TABLE [dbo].[About](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[MessageOne] [nvarchar](50) NULL,
+		[MessageTwo] [nvarchar](50) NULL,
+		[MessageThree] [nvarchar](50) NULL,
+		[ImageURLOne] [nvarchar](50) NULL,
+		[ImageURLTwo] [nvarchar](50) NULL,
+		[ImageURLThree] [nvarchar](50) NULL,
+		[EnteredOn] [datetime] NULL,
+		[EnteredBy] [int] NULL,
+		[UpdatedOn] [datetime] NULL,
+		[UpdatedBy] [int] NULL
+		) ON [PRIMARY]
+	GO
+	
+	ALTER TABLE [dbo].[About]  
+		WITH CHECK ADD CONSTRAINT [FK_About_Users] FOREIGN KEY([EnteredBy])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[About] CHECK CONSTRAINT [FK_About_Users]
+	GO
+
+	ALTER TABLE [dbo].[About]  
+		WITH CHECK ADD CONSTRAINT [FK_About_Users1] FOREIGN KEY([UpdatedBy])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[About] CHECK CONSTRAINT [FK_About_Users1]
+	GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 11/13/2014 7:58:45 PM ******/
+
+	SET ANSI_NULLS ON
+	GO
+
+	SET QUOTED_IDENTIFIER ON
+	GO
+
+	CREATE TABLE [dbo].[Users](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[Email] [nvarchar](50) NULL,
+		[FirstName] [nvarchar](50) NULL,
+		[LastName] [nvarchar](50) NULL,
+		[Password] [nvarchar](50) NULL,
+		[City] [nvarchar](50) NULL,
+		[Phone] [nvarchar](50) NULL,
+		[CurrentCollege] [nvarchar](50) NULL,
+		[ImageURL] [nvarchar](50) NULL,
+		[Description] [nvarchar](50) NULL,
+		[CreatedOn] [datetime] NULL,
+		CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (
+			PAD_INDEX = OFF, 
+			STATISTICS_NORECOMPUTE = OFF, 
+			IGNORE_DUP_KEY = OFF, 
+			ALLOW_ROW_LOCKS = ON, 
+			ALLOW_PAGE_LOCKS = ON
+			) ON [PRIMARY]
+		) ON [PRIMARY]
+	GO
+
+/****** Object:  Table [dbo].[UserReviews]    Script Date: 11/13/2014 7:58:31 PM ******/
+
+	SET ANSI_NULLS ON
+	GO
+	
+	SET QUOTED_IDENTIFIER ON
+	GO
+	
+	CREATE TABLE [dbo].[UserReviews](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[Rating] [float] NULL,
+		[Comment] [nvarchar](50) NULL,
+		[Date] [datetime] NULL,
+		[FromUserId] [int] NULL,
+		[ForUserId] [int] NULL,
+		CONSTRAINT [PK_UserReviews] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (
+			PAD_INDEX = OFF, 
+			STATISTICS_NORECOMPUTE = OFF, 
+			IGNORE_DUP_KEY = OFF, 
+			ALLOW_ROW_LOCKS = ON, 
+			ALLOW_PAGE_LOCKS = ON
+			) ON [PRIMARY]
+		) ON [PRIMARY]
+	GO
+		
+	ALTER TABLE [dbo].[UserReviews]  
+		WITH CHECK ADD CONSTRAINT [FK_UserReviews_Users] FOREIGN KEY([FromUserId])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[UserReviews] CHECK CONSTRAINT [FK_UserReviews_Users]
+	GO
+
+	ALTER TABLE [dbo].[UserReviews]  
+		WITH CHECK ADD  CONSTRAINT [FK_UserReviews_Users1] FOREIGN KEY([ForUserId])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[UserReviews] CHECK CONSTRAINT [FK_UserReviews_Users1]
+	GO
+
+/****** Object:  Table [dbo].[Postings]    Script Date: 11/13/2014 8:02:12 PM ******/
+
+	SET ANSI_NULLS ON
+	GO
+	
+	SET QUOTED_IDENTIFIER ON
+	GO
+	
+	CREATE TABLE [dbo].[Postings](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[ISBN] [nvarchar](50) NULL,
+		[Description] [nvarchar](50) NULL,
+		[Major] [nvarchar](50) NULL,
+		[Title] [nvarchar](50) NULL,
+		[Author] [nvarchar](50) NULL,
+		[Price] [nvarchar](50) NULL,
+		[Condition] [nvarchar](50) NULL,
+		[ImageURL] [nvarchar](50) NULL,
+		[EnteredOn] [nvarchar](50) NULL,
+		[UpdatedOn] [nvarchar](50) NULL,
+		[UserId] [int] NULL,
+		CONSTRAINT [PK_Postings] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (
+			PAD_INDEX = OFF, 
+			STATISTICS_NORECOMPUTE = OFF, 
+			IGNORE_DUP_KEY = OFF, 
+			ALLOW_ROW_LOCKS = ON, 
+			ALLOW_PAGE_LOCKS = ON
+			) ON [PRIMARY]
+		) ON [PRIMARY]
+	GO
+	
+	ALTER TABLE [dbo].[Postings]  
+		WITH CHECK ADD CONSTRAINT [FK_Postings_Users] FOREIGN KEY([UserId])REFERENCES [dbo].[Users] ([Id])
+	GO
+
+	ALTER TABLE [dbo].[Postings] CHECK CONSTRAINT [FK_Postings_Users]
+	GO
+
+/****** Object:  Table [dbo].[TradeRequests]    Script Date: 11/13/2014 8:03:44 PM ******/
+
+	SET ANSI_NULLS ON
+	GO
+	
+	SET QUOTED_IDENTIFIER ON
+	GO
+	
+	CREATE TABLE [dbo].[TradeRequests](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[Status] [int] NULL,
+		[RequestDate] [datetime] NULL,
+		[PostingId] [int] NULL,
+		[UserId] [int] NULL,		
+		CONSTRAINT [PK_TradeRequests] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (
+			PAD_INDEX = OFF, 
+			STATISTICS_NORECOMPUTE = OFF, 
+			IGNORE_DUP_KEY = OFF, 
+			ALLOW_ROW_LOCKS = ON, 
+			ALLOW_PAGE_LOCKS = ON
+			) ON [PRIMARY]
+		) ON [PRIMARY]
+	GO
+	
+	ALTER TABLE [dbo].[TradeRequests]  
+		WITH CHECK ADD CONSTRAINT [FK_TradeRequests_Postings] FOREIGN KEY([PostingId]) REFERENCES [dbo].[Postings] ([Id])
+	GO
+	
+	ALTER TABLE [dbo].[TradeRequests] CHECK CONSTRAINT [FK_TradeRequests_Postings]
+	GO
+	
+	ALTER TABLE [dbo].[TradeRequests]  
+		WITH CHECK ADD CONSTRAINT [FK_TradeRequests_Users] FOREIGN KEY([UserId])	REFERENCES [dbo].[Users] ([Id])
+	GO
+	
+	ALTER TABLE [dbo].[TradeRequests] CHECK CONSTRAINT [FK_TradeRequests_Users]
+	GO
